032d72d8f495b460e8fce155f218b5384cc36015
afong <adam.y.w.fong@gmail.com>
2023-11-12 12:25:51 -0800

    added logo images for Footer

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 This is my portfolio, designed using React.  It was made both as a way to practice using React and to have an easily sharable page that displays information about me, my past work, and how to contact me. 
 
-Link to site: [insert link here](https://adamywfong.github.io/react-portfolio/).
+Link to site: [https://transcendent-cascaron-a3d23e.netlify.app/](https://transcendent-cascaron-a3d23e.netlify.app/).
 
 ![Screenshot of deployed portfolio](assets/page-screenshot.jpeg)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,17 @@
+import githubMark from '../assets/github-mark.svg'
+
 export default function Footer() {
   return (
     <div>
-      <p>
-        This is a footer
-      </p>
+      <a href="https://github.com/adamywfong">
+        <img src={githubMark} className="logo" alt="github logo" />
+      </a>
+      <a href="https://www.linkedin.com/in/adam-fong-83062a73/">
+        <img src='../assets/LinkedIn.png' className="logo" alt="LinkedIn logo" />
+      </a>
+      <a href="https://www.facebook.com/adam.y.w.fong/">
+        <img src="../assets/FacebookPrimary.png" className="logo" alt="Facebook logo"/>
+      </a>
     </div>
   )
 
