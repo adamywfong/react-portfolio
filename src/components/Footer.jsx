6610bed6f4eb795a2fe981c9d7dfbf918afac4ef
afong <adam.y.w.fong@gmail.com>
2023-11-12 17:34:58 -0800

    updated image paths

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ export default function Footer() {
   return (
     <div className='justify-content-center'>
       <a href="https://github.com/adamywfong">
-        <img src='public/assets/github-mark.svg' className="logo" alt="GitHub logo" />
+        <img src='../assets/github-mark.svg' className="logo" alt="GitHub logo" />
       </a>
       <a href="https://www.linkedin.com/in/adam-fong-83062a73/">
-        <img src='public/assets/LinkedIn.png' className="logo" alt="LinkedIn logo" />
+        <img src='../assets/LinkedIn.png' className="logo" alt="LinkedIn logo" />
       </a>
       <a href="https://www.facebook.com/adam.y.w.fong/">
-        <img src="public/assets/FacebookPrimary.png" className="logo" alt="Facebook logo"/>
+        <img src="../assets/FacebookPrimary.png" className="logo" alt="Facebook logo"/>
       </a>
     </div>
   )
